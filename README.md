8df640bac2632f33e9c9bc9c00b96623c1f3ceee
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-09-18 20:24:03 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# atu-rihan-project-12
+# atu-rihan-project-12
---
+++ index.js
@@ -0,0 +1,5 @@
+const array =["shamim","hahid","habib", "amin", "shakil"];
+
+for(let i=0; array.length >0 ; i++){
+      console.log(i);
+}
